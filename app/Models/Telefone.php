ece0db65b6377e36a9446a7825b8b1548999c104
Eliel de Souza Fariã <elielrj@gmail.com>
2023-12-16 21:20:02 -0300

    Ajuste de Telefone Models

--- app/Models/Telefone.php
+++ app/Models/Telefone.php
@@ -20,9 +20,9 @@ class Telefone extends Model
     protected $allowedFields = ['ddd', 'numero'];
 
     public function __construct(
-        $id,
-        $ddd,
-        $numero,
+        $id = null,
+        $ddd = null,
+        $numero = null,
         $dataHoraDaCriacao = null,
         $deletadoEm = null
     )
@@ -42,5 +42,45 @@ class Telefone extends Model
     public function __set($key, $value)
     {
         $this->$key = $value;
+    }
+
+    public function criarTelefone()
+    {
+        $ERRO = 'Telefone::criarTelefone()';
+
+        $this->insert([
+            'id' => null,
+            'ddd' => $this->ddd ?? $ERRO,
+            'numero' => $this->numero ?? $ERRO,
+        ]);
+    }
+
+    public function buscarTelefones(int $usuarioId)
+    {
+        $data = $this->where(['usuario_id' => $usuarioId])->findAll();
+
+        $listaDeTelefones = [];
+
+        foreach ($data as $value){
+            $telafone = new Telefone();
+            $telafone->id = $data->id;
+            $telafone->ddd = $data->ddd;
+            $telafone->numero = $data->numero;
+            $telafone->dataHoraDaCriacao = $data->data_hora_da_criacao;
+            $listaDeTelefones += $listaDeTelefones;
+        }
+        return $listaDeTelefones;
+    }
+
+    public function atualizarTelefone()
+    {
+        $this->update($this->id,[
+            'ddd'=>$this->ddd,
+            'numero'=>$this->numero,
+        ]);
+    }
+    public function deletarTelefone()
+    {
+        $this->delete($this->id);
     }
 }
