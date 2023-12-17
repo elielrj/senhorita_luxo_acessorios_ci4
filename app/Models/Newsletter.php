fb8b4a522ba0fa334e305114342a2e0564f475e7
Eliel de Souza Fariã <elielrj@gmail.com>
2023-12-16 21:21:07 -0300

    Ajuste de NewslatterModels

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use App\Models\Email;
 
 class Newsletter extends Model
 {
@@ -18,12 +19,12 @@ class Newsletter extends Model
     */
     protected $table = 'newsletter';
     protected $returnType = 'object';
-    protected $allowedFields = ['nome', 'email', 'deseja_receber'];
+    protected $allowedFields = ['nome', 'email_id', 'deseja_receber'];
 
     public function __construct(
-        $id,
-        $nome,
-        $email,
+        $id = null,
+        $nome = null,
+        $email = null,
         $desejaReceber = true,
         $dataHoraDaCriacao = null,
         $deletadoEm = null
@@ -31,7 +32,7 @@ class Newsletter extends Model
     {
         $this->id = $id;
         $this->nome = $nome;
-        $this->email = $email;
+        $this->email = $email ?? new Email();
         $this->desejaReceber = $desejaReceber;
         $this->dataHoraDaCriacao = $dataHoraDaCriacao;
         $this->deletadoEm = $deletadoEm;
@@ -53,8 +54,28 @@ class Newsletter extends Model
 
         $this->insert([
             'nome' => $this->nome ?? $ERRO,
-            'email' => $this->email ?? $ERRO,
-            'deseja_receber' => $this->desejaReceber ?? $ERRO,
+            'email_id' => $this->email->id ?? $ERRO,
+            'deseja_receber' => $this->desejaReceber ?? false,
         ]);
+    }
+
+    public function buscarTodosAsNewsletter()
+    {
+        $ERRO = 'Contato::buscarTodosAsNewsletter()';
+
+        $data = $this->findAll();
+
+        $listaDeNewlatter = [];
+
+        foreach ($data as $value) {
+            $newslatter = new Newsletter();
+            $newslatter->id = $value->id ?? $ERRO;
+            $newslatter->nome = $value->nome ?? $ERRO;
+            $newslatter->desejaReceber = $value->deseja_receber ?? $ERRO;
+            $newslatter->dataHoraDaCriacao = $value->data_hora_da_criacao ?? $ERRO;
+            $newslatter->email->buscarEmail($value->email_id);
+            $listaDeNewlatter[] += $newslatter;
+        }
+        return $listaDeNewlatter;
     }
 }
